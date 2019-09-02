edd96f864d8303a1e1cdcab327fa123c77da606e
Jonnas Fonini <jonnas@sbsistemas.com.br>
2019-09-02 13:52:31 -0300

    Ajuste

--- src/Common/Tools.php
+++ src/Common/Tools.php
@@ -189,6 +189,7 @@ class Tools
         $this->certificate = $certificate;
         $this->setEnvironment($this->config->tpAmb);
         $this->contingency = new Contingency();
+        $this->soap = new SoapCurl($this->certificate);
     }
     
     /**
